591aae8bb3733e61279a370e38f1087eb0fcb9a5
WilliamChelman <william.chelman@gmail.com>
2020-01-14 17:24:40 +0100

    1 file modified

--- src/app/my-component.component.ts
+++ src/app/my-component.component.ts
@@ -17,9 +17,7 @@ export class MyComponent {
       tap(cats => console.log('getAll returns an array of cats', cats)),
       switchMap(cats => cats),
       tap(cat => console.log('Now operators are called on each cat', cat)),
-      mergeMap(cat => 
-				this.catService.getSlaves(cat.id).pipe(map(slaves => ({cat, slaves: slaves.map(slave => slave.name).join(', ')})))
-		  ),
+      mergeMap(cat => this.catService.getSlaves(cat.id).pipe(map(slaves => ({cat, slaves: slaves.map(slave => slave.name).join(', ')})))),
       tap(catContainer => console.log('Hello container', catContainer)),
       reduce((acc, current) => [...acc, current], []),
       tap(allContainers => console.log('at the end, here are all the containers', allContainers))
